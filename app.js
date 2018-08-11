ff6d462c2693f15a7d13c6ab73cf29662e695786
Caroline <vc.setiawan@gmail.com>
2018-08-11 19:09:26 +1000

    addressing cors

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-console.log('ola')
+
 urls = [
     {
-        url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=404&s=8329&t=3730&embedded=',
+        // url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=404&s=8329&t=3730&embedded=',
+        url: 'https://m95pp3sq2c.execute-api.us-east-1.amazonaws.com/vball/short-cuts/',
         container: '#container1'
     },
     {
-        url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=405&s=8329&t=3513&embedded=',
+        //url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=405&s=8329&t=3513&embedded=',
+        url: 'https://m95pp3sq2c.execute-api.us-east-1.amazonaws.com/vball/hotline-bling/',
         container: '#container2'
     },
     {
-        url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=406&s=8329&t=3711&embedded=',
+        //url: 'https://results.sportskeepglobal.com/vic--sandstorm-altona/stadium-team-fixture.php?sp=25&g=406&s=8329&t=3711&embedded=',
+        url: 'https://m95pp3sq2c.execute-api.us-east-1.amazonaws.com/vball/hotline/',
         container: '#container3'
     }
 ]
@@ -26,8 +29,7 @@ urls.forEach(function(url) {
         $(url.container).find('.time').text($(response).find('#ladder tr:nth-child(2) td:nth-child(5)').text())
         // court
         $(url.container).find('.court').text($(response).find('#ladder tr:nth-child(2) td:nth-child(6)').text())
-    })
-    
+    })  
 })
 
 
